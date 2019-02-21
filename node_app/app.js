6a97170779076b56d4c2ef784f30e93323dba2da
Zayne  Berndt <berndt.zayne@gmail.com>
2019-02-21 12:41:59 +0000

    auth wip

--- node_app/app.js
+++ node_app/app.js
@@ -1,8 +1,26 @@
-const express = require('express'),
-  app = express();
+const express = require("express");
+const app = express();
+module.exports = app;
+const authmodule = require('./modules/authmodule.js')
+
 
 app.get('/api/version', function(req, res) {
-  res.json({ version: '1.0' })
+  authmodule.checkAuth(req.headers.token).then(function(userdata){
+    //authed
+    res.json({ version: '1.0' })
+  }).catch(function(){
+    //not authed
+      res.json({ action: 'redirect=login' })
+  })
+})
+
+
+
+app.post('/api/login', function(req, body, res) {
+  //auth here
+
+  var authToken = 'xasdsa-xsaxas-xsaxsa-xsaxas'
+  res.json({ token: authToken })
 })
 
 app.listen(80)
